62333bc2ec04d44a9c0496868290c8de9e57280c
heinrich4551-netizen <heinrich4551@gmail.com>
2026-09-03 08:02:03 -0400

    Make template cTab UI controls self-contained

--- mission-template/RHD_LifeCore.TEMPLATE/core/ui/ctab.hpp
+++ mission-template/RHD_LifeCore.TEMPLATE/core/ui/ctab.hpp
@@ -1,27 +1,91 @@
 /*
     RHD cTab PLAYER TABLET UI
     ---------------------------------------------------------------------------
-    This file contains only the RHD controls placed on cTab's tablet display.
-
-    BEGINNER EDITORS:
-    - You normally do not need to change this file.
-    - Change page text/buttons in core/ui/fn_ctabPage.sqf.
-    - Change tablet layout here only if you understand Arma UI coordinates.
-
-    Each control has a unique IDC in the 9800+ range so it does not collide
-    with normal RHD dialogs.
-    ---------------------------------------------------------------------------
+    Self-contained RHD UI controls. This file does not inherit from global
+    Rsc* classes, preventing config-order/base-class errors.
 */
 
-// Tablet background panel.
-class RHD_CTAB_Background: RscText
+class RHD_CTAB_RscText
+{
+    access = 0;
+    type = 0;
+    idc = -1;
+    style = 0;
+    x = 0;
+    y = 0;
+    w = 0;
+    h = 0;
+    font = "PuristaMedium";
+    sizeEx = 0.04;
+    text = "";
+    colorText[] = {1,1,1,1};
+    colorBackground[] = {0,0,0,0};
+    shadow = 0;
+};
+
+class RHD_CTAB_RscStructuredText
+{
+    access = 0;
+    type = 13;
+    idc = -1;
+    style = 0;
+    x = 0;
+    y = 0;
+    w = 0;
+    h = 0;
+    font = "PuristaMedium";
+    size = 0.04;
+    sizeEx = 0.04;
+    text = "";
+    colorText[] = {1,1,1,1};
+    colorBackground[] = {0,0,0,0};
+    shadow = 0;
+};
+
+class RHD_CTAB_RscListbox
+{
+    access = 0;
+    type = 5;
+    idc = -1;
+    style = 16;
+    x = 0;
+    y = 0;
+    w = 0;
+    h = 0;
+    font = "PuristaMedium";
+    sizeEx = 0.04;
+    rowHeight = 0.04;
+    colorText[] = {1,1,1,1};
+    colorBackground[] = {0,0,0,0.3};
+    colorSelect[] = {1,1,1,1};
+    colorSelectBackground[] = {0.2,0.2,0.2,1};
+};
+
+class RHD_CTAB_RscButton
+{
+    access = 0;
+    type = 1;
+    idc = -1;
+    style = 2;
+    x = 0;
+    y = 0;
+    w = 0;
+    h = 0;
+    font = "PuristaMedium";
+    sizeEx = 0.04;
+    text = "";
+    colorText[] = {1,1,1,1};
+    colorBackground[] = {0,0,0,0.5};
+    colorBackgroundActive[] = {0.2,0.2,0.2,0.8};
+};
+
+class RHD_CTAB_Background: RHD_CTAB_RscText
 {
     idc = 9800;
     colorBackground[] = {.015,.025,.035,.82};
 };
 
-// Tablet title/header.
-class RHD_CTAB_Title: RscText
+class RHD_CTAB_Title: RHD_CTAB_RscText
 {
     idc = 9801;
     sizeEx = .027;
@@ -29,8 +93,7 @@ class RHD_CTAB_Title: RscText
     colorText[] = {.90,.96,1,1};
 };
 
-// Page navigation buttons.
-class RHD_CTAB_Nav: RscButton
+class RHD_CTAB_Nav: RHD_CTAB_RscButton
 {
     sizeEx = .018;
     colorBackground[] = {.05,.20,.28,.95};
@@ -38,8 +101,7 @@ class RHD_CTAB_Nav: RscButton
     colorText[] = {1,1,1,1};
 };
 
-// Main page content area.
-class RHD_CTAB_Content: RscStructuredText
+class RHD_CTAB_Content: RHD_CTAB_RscStructuredText
 {
     idc = 9807;
     colorBackground[] = {.01,.02,.03,.88};
@@ -47,8 +109,7 @@ class RHD_CTAB_Content: RscStructuredText
     sizeEx = .020;
 };
 
-// List control used by jobs and shop pages.
-class RHD_CTAB_List: RscListbox
+class RHD_CTAB_List: RHD_CTAB_RscListbox
 {
     idc = 9808;
     sizeEx = .018;
@@ -57,8 +118,7 @@ class RHD_CTAB_List: RscListbox
     colorSelectBackground[] = {.08,.30,.40,1};
 };
 
-// Primary/secondary page action buttons.
-class RHD_CTAB_Button: RscButton
+class RHD_CTAB_Button: RHD_CTAB_RscButton
 {
     sizeEx = .017;
     colorBackground[] = {.06,.28,.36,.96};
@@ -66,8 +126,7 @@ class RHD_CTAB_Button: RscButton
     colorText[] = {1,1,1,1};
 };
 
-// Small status line at the bottom of the tablet page.
-class RHD_CTAB_Status: RscText
+class RHD_CTAB_Status: RHD_CTAB_RscText
 {
     idc = 9811;
     sizeEx = .016;
