70eba73e9e37d9a686d7cfe05cb462d0c78870e0
heinrich4551-netizen <heinrich4551@gmail.com>
2026-09-03 08:34:08 -0400

    Fix mission-side cTab UI base classes

--- core/ui/ctab.hpp
+++ core/ui/ctab.hpp
@@ -3,25 +3,95 @@
     ---------------------------------------------------------------------------
     This file contains only the RHD controls placed on cTab's tablet display.
 
-    BEGINNER EDITORS:
-    - You normally do not need to change this file.
-    - Change page text/buttons in core/ui/fn_ctabPage.sqf.
-    - Change tablet layout here only if you understand Arma UI coordinates.
-
-    Each control has a unique IDC in the 9800+ range so it does not collide
-    with normal RHD dialogs.
+    The mission-side template is intentionally self-contained. It does not
+    inherit from global Rsc* classes, which can be unavailable depending on
+    config include/load order.
     ---------------------------------------------------------------------------
 */
 
+class RHD_MissionRscText
+{
+    access = 0;
+    type = 0;
+    idc = -1;
+    style = 0;
+    x = 0;
+    y = 0;
+    w = 0;
+    h = 0;
+    font = "PuristaMedium";
+    sizeEx = 0.04;
+    text = "";
+    colorText[] = {1,1,1,1};
+    colorBackground[] = {0,0,0,0};
+    shadow = 0;
+};
+
+class RHD_MissionRscStructuredText
+{
+    access = 0;
+    type = 13;
+    idc = -1;
+    style = 0;
+    x = 0;
+    y = 0;
+    w = 0;
+    h = 0;
+    font = "PuristaMedium";
+    size = 0.04;
+    sizeEx = 0.04;
+    text = "";
+    colorText[] = {1,1,1,1};
+    colorBackground[] = {0,0,0,0};
+    shadow = 0;
+};
+
+class RHD_MissionRscListbox
+{
+    access = 0;
+    type = 5;
+    idc = -1;
+    style = 16;
+    x = 0;
+    y = 0;
+    w = 0;
+    h = 0;
+    font = "PuristaMedium";
+    sizeEx = 0.04;
+    rowHeight = 0.04;
+    colorText[] = {1,1,1,1};
+    colorBackground[] = {0,0,0,0.3};
+    colorSelect[] = {1,1,1,1};
+    colorSelectBackground[] = {0.2,0.2,0.2,1};
+};
+
+class RHD_MissionRscButton
+{
+    access = 0;
+    type = 1;
+    idc = -1;
+    style = 2;
+    x = 0;
+    y = 0;
+    w = 0;
+    h = 0;
+    font = "PuristaMedium";
+    sizeEx = 0.04;
+    text = "";
+    colorText[] = {1,1,1,1};
+    colorBackground[] = {0,0,0,0.5};
+    colorBackgroundActive[] = {0.2,0.2,0.2,0.8};
+};
+
 // Tablet background panel.
-class RHD_CTAB_Background: RscText
+class RHD_CTAB_Background: RHD_MissionRscText
 {
     idc = 9800;
     colorBackground[] = {.015,.025,.035,.82};
 };
 
 // Tablet title/header.
-class RHD_CTAB_Title: RscText
+class RHD_CTAB_Title: RHD_MissionRscText
 {
     idc = 9801;
     sizeEx = .027;
@@ -30,7 +100,7 @@ class RHD_CTAB_Title: RscText
 };
 
 // Page navigation buttons.
-class RHD_CTAB_Nav: RscButton
+class RHD_CTAB_Nav: RHD_MissionRscButton
 {
     sizeEx = .018;
     colorBackground[] = {.05,.20,.28,.95};
@@ -39,7 +109,7 @@ class RHD_CTAB_Nav: RscButton
 };
 
 // Main page content area.
-class RHD_CTAB_Content: RscStructuredText
+class RHD_CTAB_Content: RHD_MissionRscStructuredText
 {
     idc = 9807;
     colorBackground[] = {.01,.02,.03,.88};
@@ -48,7 +118,7 @@ class RHD_CTAB_Content: RscStructuredText
 };
 
 // List control used by jobs and shop pages.
-class RHD_CTAB_List: RscListbox
+class RHD_CTAB_List: RHD_MissionRscListbox
 {
     idc = 9808;
     sizeEx = .018;
@@ -58,7 +128,7 @@ class RHD_CTAB_List: RscListbox
 };
 
 // Primary/secondary page action buttons.
-class RHD_CTAB_Button: RscButton
+class RHD_CTAB_Button: RHD_MissionRscButton
 {
     sizeEx = .017;
     colorBackground[] = {.06,.28,.36,.96};
@@ -67,7 +137,7 @@ class RHD_CTAB_Button: RscButton
 };
 
 // Small status line at the bottom of the tablet page.
-class RHD_CTAB_Status: RscText
+class RHD_CTAB_Status: RHD_MissionRscText
 {
     idc = 9811;
     sizeEx = .016;
